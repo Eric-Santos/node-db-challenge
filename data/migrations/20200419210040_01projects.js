aa503edca8f11479c4822592fbdeefcf4024fe8e
eric <eric355402@yahoo.com>
2020-04-20 18:57:55 -0500

    having issues seeding projects table

--- data/migrations/20200419210040_01projects.js
+++ data/migrations/20200419210040_01projects.js
@@ -1,45 +1,43 @@
 exports.up = function (knex) {
-  exports.up = function (knex) {
-    return knex.schema
-      .createTable("project", (tbl) => {
-        tbl.increments();
-        tbl.boolean("completed", false).notNullable();
-        tbl.text("project_name", 128).unique().notNullable();
-        tbl.text("description");
-      })
-      .createTable("task", (tbl) => {
-        tbl.increments();
-        tbl.boolean("completed", false).notNullable();
-        tbl.text("description", 128).notNullable().unique();
-        tbl.text("notes");
-        tbl
-          .integer("project_id")
-          .unsigned()
-          .notNullable()
-          .references("id")
-          .inTable("project")
-          .onUpdate("CASCADE")
-          .onDelete("CASCADE");
-      })
-      .createTable("resource", (tbl) => {
-        tbl.increments();
-        tbl.text("resource_name", 128).unique();
-        tbl.text("description");
-        tbl
-          .integer("resource_id")
-          .unsigned()
-          .notNullable()
-          .references("id")
-          .inTable("project")
-          .onUpdate("CASCADE")
-          .onDelete("CASCADE");
-      });
-  };
+  return knex.schema
+    .createTable("projects", (tbl) => {
+      tbl.increments();
+      tbl.boolean("completed", false).notNullable();
+      tbl.text("project_name", 128).unique().notNullable();
+      tbl.text("description");
+    })
+    .createTable("task", (tbl) => {
+      tbl.increments();
+      tbl.boolean("completed", false).notNullable();
+      tbl.text("description", 128).notNullable().unique();
+      tbl.text("notes");
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    })
+    .createTable("resource", (tbl) => {
+      tbl.increments();
+      tbl.text("resource_name", 128).unique();
+      tbl.text("description");
+      tbl
+        .integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    });
 };
 
 exports.down = function (knex) {
   return knex.schema
     .dropTableIfExists("resource")
     .dropTableIfExists("task")
-    .dropTableIfExists("project");
+    .dropTableIfExists("projects");
 };
